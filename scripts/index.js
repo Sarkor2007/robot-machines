a7843497e85dbc65533ee88da847a3e3a65c164a
SArkor <abilkosimovsarkor@gmail.com>
2023-08-19 22:43:22 +0500

    feat: complete desktop version

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
 console.log('Welcome')
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3.3,
+    spaceBetween: 30,
+});
+
+var swiper = new Swiper(".mySwiperTwo", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+});
+
+
+
+let dropdownHead = document.querySelectorAll('.questions__item-header');
+let dropdownBody = document.querySelectorAll('.questions__item-body');
+
+for (let i = 0; i < dropdownHead.length; i++) {
+    dropdownHead[i].addEventListener('click', () => {
+        dropdownBody[i].classList.toggle('active');
+    })
+}
+
+dropdownHead.addEventListener('click', () => {
+    dropdownBody.classList.toggle('active');
+    console.log('hello')
+})
