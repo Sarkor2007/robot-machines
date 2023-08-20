832f753f4430cdcf6b88ffb767f74330e9e611bf
SArkor <abilkosimovsarkor@gmail.com>
2023-08-20 23:25:38 +0500

    feat: complete desktop all pages

--- scripts/index.js
+++ scripts/index.js
@@ -12,18 +12,28 @@ var swiper = new Swiper(".mySwiperTwo", {
     spaceBetween: 25,
 });
 
+var swiper = new Swiper(".mySwiperThree", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        // disableOnInteraction: false,
+    },
+});
+
 
 
 let dropdownHead = document.querySelectorAll('.questions__item-header');
 let dropdownBody = document.querySelectorAll('.questions__item-body');
 
 for (let i = 0; i < dropdownHead.length; i++) {
-    dropdownHead[i].addEventListener('click', () => {
+    dropdownHead[i]?.addEventListener('click', () => {
         dropdownBody[i].classList.toggle('active');
     })
 }
 
-dropdownHead.addEventListener('click', () => {
+dropdownHead?.addEventListener('click', () => {
     dropdownBody.classList.toggle('active');
     console.log('hello')
 })
